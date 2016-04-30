750f39a068e8e003d0601f10a27f6c459f124f0d
midnightapplepie <midnightapplepie@gmail.com>
2016-04-29 19:37:45 -0700

    added pictures

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,36 +2,15 @@ var damn = "damn"
 var fs = require('fs');
 var parse = require('csv-parse');
 
-var data = {
-	var MenuItem = function(array){
-		var price = array[5].replace(/\$/g,"").split("/");
-		var lunch_price = array[9];
-		if(price.length > 1){
+function Menu(){
 
-			this.sizes = true;
-			this.price = {s: price[0]*1,m: price[1]*1, l:price[2]*1 }
-		}else if(price[0]*1){
-			this.price = price[0]*1;
-		}else{
-			this.price = price[0];
-		}
-
-		this.spicy = array[0] == ""? false : true;
-		this.category_id = array[1];
-		this.category_name = array[2];
-		this.vegetarian = array[3] ==""?false : true;
-		this.name = array[4];
-		this.lunch = array[8] == ""? false : true;
-		this.lunch_price = array[9] != ""? array[9].replace(/\$/g,"") * 1: false;
-		this.count = array[10] == ""? false : array[10]*1;
-		this.description = array[11]; 
-	};
-
-	menu : {
+	this.structure = {
 		"appetizer":[],
 		"soup":[],
 		"seafood":[],
-		"beef_lamb_pork":[],
+		"beef":[],
+		"lamb":[],
+		"pork":[],
 		"chicken":[],
 		"vegetables":[],
 		"mein_fun":[],
@@ -39,39 +18,84 @@ var data = {
 		"rice":[],
 		"lunch_groups":{
 			"seafood":[],
-			"beef_lamb_pork":[],
+			"beef":[],
+			"lamb":[],
+			"pork":[],
 			"chicken":[],
 			"vegetables":[],
 			"mein_fun":[],
 			"fried_rice":[]
 		}
-	},
+	};
 
-	generateMenu : function(){
-		var parser = parse({column: true},function(err,data){
-			var items = data;
-			items.forEach(function(e,idx,arr){
-				var ele = new MenuItem(e)
+	this.generateMenu = function(data,self){
+
+		var MenuItem = function(array){
+			var price = array[5].replace(/\$/g,"").split("/");
+			var lunch_price = array[9];
+			if(price.length > 1){
+
+				this.sizes = true;
+				this.price = {s: price[0]*1,m: price[1]*1, l:price[2]*1 }
+			}else if(price[0]*1){
+				this.price = price[0]*1;
+			}else{
+				this.price = price[0];
+			}
+
+			this.spicy = array[0] == ""? false : true;
+			this.category_id = array[1];
+			this.category_name = array[2];
+			this.vegetarian = array[3] ==""?false : true;
+			this.name = array[4];
+			this.lunch = array[8] == ""? false : true;
+			this.lunch_price = array[9] != ""? array[9].replace(/\$/g,"") * 1: false;
+			this.count = array[10] == ""? false : array[10]*1;
+			this.description = array[11]; 
+		};
+
+		data.forEach(function(e,idx,arr){
+			var ele = new MenuItem(e)
+			try{
+				self.structure[ele.category_id].push(ele);
+			}
+			catch(err){
+				console.log(idx)
+			}
+			if(ele.lunch){
 				try{
-					this.menu[ele.category_id].push(ele);
+					self.structure["lunch_groups"][ele.category_id].push(ele)
 				}
 				catch(err){
-					console.log(idx)
+					console.log(idx);
 				}
-				if(ele.lunch){
-					try{
-						this.menu["lunch_groups"][ele.category_id].push(ele)
-					}
-					catch(err){
-						console.log(idx);
-					}
-				}
-			});
-		});
+			}
 
-		fs.createReadStream('./assets/menu.csv').pipe(parser);
-	}
+			if(idx+1 == arr.length){
+				// console.log(self)
+			}
+		});
+	};
+
+	this.loadFile = function(){
+		var self = this;
+		fs.readFile('./assets/menu.csv',function(err,data){
+			if(err){
+				console.log(err);
+			}else{
+				parse(data,{column: true},function(parseErr,parseData){
+					if(parseErr){
+						console.log(parseErr);
+					}else{
+						self.generateMenu(parseData,self)
+					}
+				})
+			}
+		});
+	};
+
 }
 
+var menu = new Menu();
 
-module.exports = data;
+module.exports = menu;
--- xlApp.js
+++ xlApp.js
@@ -2,11 +2,26 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var exphbs = require('express-handlebars');
-var d = require("./assets/js/db.js");
+var sassMiddleware = require('node-sass-middleware');
 
 
+//dass setting
+app.use(sassMiddleware({
+	src: __dirname + "/assets/css/sass",
+	dest: __dirname + "/assets/css/sass_compiled",
+	debug: true,
+	outputStyle: 'compressed',
+	prefix:"/sass_compiled"
+}))
+
+//adding menu
+app.locals.menu = require("./assets/js/menu.js");
+app.locals.menu.loadFile();
+
+//add assets folder
 app.use(express.static(path.join(__dirname + "/assets")));
 
+//view setting
 app.engine('hbs',exphbs({defaultLayout: 'layout'}));
 app.set('view engine', 'hbs');
 app.set('views',__dirname + '/views');
@@ -15,6 +30,10 @@ app.get("/",function(req,res){
 	res.render("home");
 })
 
+app.get("/menu",function(res,res){
+	console.log(app.locals.menu.structure.beef)
+	res.render("menu",{data: app.locals.menu.structure});
+})
 
 
 
